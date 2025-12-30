24dce158ebed9daa6db2639a8203007f7e67a705
SAKSHAM SHRIVASTAV <167529365+sakshamfromgithub@users.noreply.github.com>
2025-12-30 16:25:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -170,11 +170,3 @@ window.login = async function () {
     alert(error.message);
   }
 };
-
-window.signup = signup;
-window.login = login;
-window.openModal = openModal;
-window.closeModal = closeModal;
-window.showLogin = showLogin;
-window.showSignup = showSignup;
-
